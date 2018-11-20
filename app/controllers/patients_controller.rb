e966ff8fd8b457aa6b48710fd6065a0b9b373d7d
Harlan Evans <harlanevans5@gmail.com>
2018-11-20 15:29:13 -0700

    initial commit

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,39 @@
+class PatientsController < ApplicationController
+  before_action :set_patient, only: [:show, :destroy]
+
+  
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      redirect_to @patients
+    else
+      render :new
+    end
+  end
+
+    def destroy
+      @patient.destroy
+      redirect_to patients_path
+    end
+
+      private
+
+      def patient_params
+        params.require(:patient).permit(:first_name, :last_name)
+      end
+
+      def set_patient
+        @patient = Patient.find(params[:id])
+      end
+end
